c11c1b5590bd392909c3c9b93a7408dd429838ab
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2020-12-15 19:51:56 -0500

    DOM manipulation practice

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 'use  strict';
 
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').value);
