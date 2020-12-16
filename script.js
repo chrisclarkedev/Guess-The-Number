9e2f45aefa0ed47000f95b4e2af14dd122a73ca5
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2020-12-16 09:40:53 -0500

    starting of style sheet

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ console.log(document.querySelector('.message').textContent);
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
-console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
