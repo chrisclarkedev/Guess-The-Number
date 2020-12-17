10508d93750d6b08b00c3f9e1042099d4a102d21
chrisclarkedev <cclarke8904@gmail.com>
2020-12-16 21:35:11 -0500

    random number generator logic

--- script.js
+++ script.js
@@ -7,8 +7,12 @@
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+// Defining Secret Number
+// Secret Number will be randomly generized and when it does it will not exceed 20
+const number = Math.trunc(Math.random() * 20) + 1;
 
 // input in input box is converted into a number then checked and logged ot to the console
 document.querySelector('.check').addEventListener('click', function () {
@@ -18,5 +22,7 @@ document.querySelector('.check').addEventListener('click', function () {
   // if input box has nothing in it, check if anything is there then display message
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number Inputed!';
+
+    // Implementation of Game Logic
   }
 });
