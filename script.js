3980d7a54ed98eb891314f60ade5670bd347faf6
chrisclarkedev <cclarke8904@gmail.com>
2020-12-16 21:03:45 -0500

    change of query

--- script.js
+++ script.js
@@ -7,8 +7,8 @@
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
 
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
 
 document.querySelector('.check').addEventListener('click', function () {
   console.log(document.querySelector('.guess').value);
