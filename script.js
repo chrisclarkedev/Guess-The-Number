55ce3fb13722704c90cba9374dee32ee39a1cc56
chrisclarkedev <cclarke8904@gmail.com>
2020-12-16 21:11:59 -0500

    check what type guess value is

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
-  document.querySelector('.message').textContent = '🎉 Correct Number!';
+  if (!guess) {
+  }
 });
